93083f0e570d4c5dca05e9d24304eae21fc4adef
おかたいち <>
2019-04-06 23:07:05 +0900

    vue game app 2nd

---
+++ src/components/Score.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="score">
+    <ul class="score-list">
+      <li class="score-list-item" v-for="sc in scoreList" :key="sc.id">
+        {{ sc.id+1 }}回目：{{ sc.slotValues }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'score',
+  data () {
+    return {
+      scoreList: this.$store.getters.getResult
+    }
+  }
+}
+</script>
+
+<style>
+.core-list {
+  list-style: none;
+  padding-left: 0
+}
+.score-list-item {
+  display: block;
+  font-size: 2em;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px;
+}
+</style>
---
+++ src/components/Start.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="start-button" class="start-button-form">
+    <input class="start-button" type="button" value="START" v-on:click="$emit('slot-start', true)">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'start'
+}
+</script>
+
+<style>
+.start-button-form {
+  display: block;
+  margin: 10px auto;
+}
+.start-button {
+  display: block;
+  margin: 0 auto;
+  padding: 0 130px;
+  text-align: center;
+  background-color: greenyellow;
+  font-size: 2em;
+}
+</style>
