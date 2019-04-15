f09e0cecddaca14ad557aab2abae2295ed07f495
おかたいち <>
2019-04-15 13:59:52 +0900

    vue transition

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,7 +5,9 @@
     <SlotBase v-on:slot-selected-value="inputResult" v-bind:stateflg="this.$store.getters.getStateflg"></SlotBase>
     <SlotBase v-on:slot-selected-value="inputResult" v-bind:stateflg="this.$store.getters.getStateflg"></SlotBase>
     <!-- <Modal v-if="this.$store.getters.getModalflg" v-bind:result="result" v-on:close="offModal"></Modal> -->
-    <Modal v-if="this.$store.getters.getModalflg" v-on:close="offModal">{{ this.result }}</Modal>
+    <transition>
+      <Modal v-if="this.$store.getters.getModalflg" v-on:close="offModal">{{ this.result }}</Modal>
+    </transition>
   </div>
 </template>
 
@@ -81,3 +83,22 @@ export default {
   }
 }
 </script>
+<style>
+.v-enter-active {
+  animation: bounce-in .5s;
+}
+.v-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
